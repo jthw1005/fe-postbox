21183af4f578edb55996d1e3681202350b568fe4
bukim0329 <bukim0329@gmail.com>
2022-03-09 21:20:42 +0900

    refactor: Data type에 따른 반환 분리 구현

--- public/javascript/objFactory.js
+++ public/javascript/objFactory.js
@@ -2,16 +2,16 @@ import { Village } from "./Village.js";
 import { MailBox } from "./MailBox.js";
 import { fetchData } from "./util.mjs";
 
-const creatObject = async (url) => {
-  const jsonData = await fetchData(url);
-  return jsonData.data.map((element) => {
-    switch (element.dataType) {
-      case "Village":
-        return new Village(element.name, element.coor);
-      case "MailBox":
-        return new MailBox(element.coor);
-    }
-  });
+const creatObject = async (dataType) => {
+  const jsonData = await fetchData(dataType);
+  switch (dataType) {
+    case "villages":
+      return jsonData.villages.map(
+        (element) => new Village(element.name, element.coor)
+      );
+    case "mailBox":
+      return jsonData.mailBox.map((element) => new MailBox(element.coor));
+  }
 };
 
 export { creatObject };
