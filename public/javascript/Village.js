34923c74aae8ecc60992633d900db62cd7388e8b
bukim0329 <bukim0329@gmail.com>
2022-03-09 21:06:41 +0900

    feat: 포함관계확인기능 메서드 추가

--- public/javascript/Village.js
+++ public/javascript/Village.js
@@ -10,4 +10,16 @@ export class Village {
     this.innerVillages = new Set();
     this.mailBox = null;
   }
+
+  isParent(village) {
+    if (
+      this.leftTop.x < village.leftTop.x &&
+      this.leftTop.y < village.leftTop.y &&
+      this.rightBot.x > village.rightBot.x &&
+      this.rightBot.y > village.rightBot.y
+    ) {
+      return true;
+    }
+    return false;
+  }
 }
