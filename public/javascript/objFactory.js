02f11ec5253d663e889f8bdf20e5f153d557be55
bukim0329 <bukim0329@gmail.com>
2022-03-09 21:22:14 +0900

    feat: Map tree 생성 기능 구현

--- public/javascript/objFactory.js
+++ public/javascript/objFactory.js
@@ -14,4 +14,54 @@ const creatObject = async (dataType) => {
   }
 };
 
-export { creatObject };
+const buildVillageTree = (villageData) => {
+  const town = villageData.shift();
+  const search = (parent, village) => {
+    if (parent.isEmpty()) {
+      parent.innerVillages.add(village);
+      return;
+    }
+    for (const child of [...parent.innerVillages]) {
+      if (child.isParent(village)) {
+        search(child, village);
+        return;
+      }
+      if (village.isParent(child)) {
+        parent.innerVillages.add(village);
+        parent.innerVillages.delete(child);
+        search(village, child);
+        return;
+      }
+    }
+    parent.innerVillages.add(village);
+  };
+  villageData.forEach((village) => {
+    search(town, village);
+  });
+
+  return town;
+};
+
+const insertMailBox = (town, mailBoxData) => {
+  const search = (village, mailBox) => {
+    if (village.isEmpty()) {
+      village.mailBox = mailBox;
+      mailBox.village = village;
+      return;
+    }
+    for (const child of [...village.innerVillages]) {
+      if (child.isParent(mailBox)) {
+        search(child, mailBox);
+        return;
+      }
+    }
+    village.mailBox = mailBox;
+    mailBox.village = village;
+  };
+  mailBoxData.forEach((mailBox) => {
+    search(town, mailBox);
+  });
+  return town;
+};
+
+export { creatObject, buildVillageTree, insertMailBox };
