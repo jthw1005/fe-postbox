8ad69fa3e072418e0fec6b3aafb3d10809d1e1a1
bukim0329 <bukim0329@gmail.com>
2022-03-13 15:54:14 +0900

    refactor: 예외 처리 및 코드 단순화

--- public/javascript/mergeSort.js
+++ public/javascript/mergeSort.js
@@ -1,34 +1,24 @@
 const mergeSort = (dataArr) => {
-  if (dataArr.length === 1) {
+  if (dataArr.length < 2) {
     return dataArr;
   }
   const center = Math.floor(dataArr.length / 2);
   const left = mergeSort(dataArr.slice(0, center));
   const right = mergeSort(dataArr.slice(center));
 
-  return myMerge(left, right);
+  return mergeTwoArrayInOrder(left, right);
 };
 
-const myMerge = (left, right) => {
+const mergeTwoArrayInOrder = (left, right) => {
   let leftIdx = 0;
   let rightIdx = 0;
-  let result = [];
+  const result = [];
   while (leftIdx < left.length && rightIdx < right.length) {
     const [_leftName, leftSize] = left[leftIdx];
     const [_rightName, rightSize] = right[rightIdx];
-    if (leftSize < rightSize) {
-      result.push(left[leftIdx]);
-      leftIdx += 1;
-    } else {
-      result.push(right[rightIdx]);
-      rightIdx += 1;
-    }
+    result.push(leftSize < rightSize ? left[leftIdx++] : right[rightIdx++]);
   }
-  if (leftIdx === left.length) {
-    result = [...result, ...right.slice(rightIdx)];
-  } else {
-    result = [...result, ...left.slice(leftIdx)];
-  }
+  leftIdx === left.length ? result.push(...right.slice(rightIdx)) : result.push(...left.slice(leftIdx));
 
   return result;
 };
